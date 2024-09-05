fdd59166af9c5bc35d20ce22c798ed4972cf3269
Mohamed Nour Ben Hassouna <roadtogmhsouna@gmail.com>
2024-09-05 09:09:03 +0100

    Create managers-with-at-least-5-direct-reports.sql

---
+++ managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,92 @@
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int)
+Truncate table Employee
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 'None')
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101')
+--******************************************************************************************************************************-- 
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+--******************************************************************************************************************************-- 
+
+--****************** SOLUTION 1 :  ************************************************************************************************************-- 
+
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+);
+
+--****************** SOLUTION 2 :  ************************************************************************************************************-- 
+
+WITH DirectReportsCount AS (
+    SELECT managerId, COUNT(*) AS numDirectReports
+    FROM Employee
+    GROUP BY managerId
+)
+SELECT e.name
+FROM Employee e
+JOIN DirectReportsCount d ON e.id = d.managerId
+WHERE d.numDirectReports >= 5;
+
+
+--****************** SOLUTION 3  :  ************************************************************************************************************-- 
+
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+);
+
+--******************************************************************************************************************************-- 
+
